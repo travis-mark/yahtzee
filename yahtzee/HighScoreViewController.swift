1ffb0159cb347596c069259b4af1eb74ac1df2d6
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-07-07 22:05:13 -0400

    High score animation

--- yahtzee/HighScoreViewController.swift
+++ yahtzee/HighScoreViewController.swift
@@ -15,8 +15,26 @@ class HighScoreViewController: UIViewController {
     @IBOutlet weak var highScore4: UILabel!
     @IBOutlet weak var highScore5: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if animated {
+            let controls = [highScore1, highScore2, highScore3, highScore4, highScore5].compactMap({ $0 })
+            let x = controls.map { $0.frame.origin.x }
+            let y = controls.map { $0.frame.origin.y }
+            let width = controls.map { $0.frame.size.width }
+            let height = controls.map { $0.frame.size.height }
+            let bottom = view.frame.size.height + 100
+            let t0 = [0.0, 0.3, 0.55, 0.75, 0.90]
+            let dt = [0.3, 0.25, 0.2, 0.15, 0.1]
+            for (i, control) in controls.enumerated() {
+                control.frame = CGRect(x: x[i], y: bottom, width: width[i], height: height[i])
+                UIView.animate(withDuration: dt[i], delay: t0[i]) {
+                    control.frame = CGRect(x: x[i], y: y[i], width: width[i], height: height[i])
+                }
+            }
+        }
+        
         loadData()
     }
     
